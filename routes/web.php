9cf38c2974e419ff6eb20aeed0df41e8f02a4441
listyantidewi1 <listyantidewi@gmail.com>
2023-01-18 13:20:13 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return 'Home page';
+})->name('home.index');
+
+Route::get('/contact', function () {
+    return 'Contact page';
+})->name('home.contact');
+
+Route::get('/posts/{id}', function ($id) {
+    return 'Blog post ' . $id;
+})
+    // ->where([
+    //     'id' => '[0-9]+'
+    // ])
+    ->name('posts.show');
+
+
+
+Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
+    return 'Posts from ' . $daysAgo . ' days ago';
+})->name('posts.recent.index');
+
+// Route::get('/posts/2', function () {
+//     return 'Blog post 2';
+// });
